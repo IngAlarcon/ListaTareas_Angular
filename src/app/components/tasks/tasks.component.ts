3e59930d64a6f077eb7d6cd6b8f025bdce794a05
Chango Alarcon <ing.alarconwalter@gmail.com>
2022-01-29 02:29:09 -0300

    Priemer Commit de Lista de Items

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+// Importando mi simulador de base de datos 
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
